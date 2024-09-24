bf8b6d8953320ef4f41eb2ef419421b7fe822e74
n1tebb <kolasnikovegor@gmai.com>
2024-09-24 13:38:40 +0500

    123

--- para2409/main.cpp
+++ para2409/main.cpp
@@ -1,14 +1,20 @@
-//
-//  main.cpp
-//  para2409
-//
-//  Created by Egor on 24.09.2024.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main () {
+    int num = 0;
+    int summ = 0;
+    
+    
+    std::cout << "введите числа(0 = завешение): ";
+    while (true){
+        std::cin >> num;
+        if (num == 0) {
+            break;
+        }
+        summ += num;
+    }
+    
+    std::cout << "сумме всех чиcел = " << summ << "\n";
+    
     return 0;
 }
